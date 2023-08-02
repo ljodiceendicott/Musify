47b8bbde3c149ba135bb940d0b4c3e0af3950ce9
ljodice <>
2023-08-02 00:41:20 -0400

    Layout complete and start of supabase Itegration

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 
 import Box from "./Box";
+import Library from "./Library";
 import SidebarItem from "./SidebarItem";
 
 interface SidebarProps{
@@ -45,9 +46,10 @@ const Sidebar: React.FC<SidebarProps>= ({
 
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    Song Library
+                    <Library/>
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
         </div>
     );
 }
